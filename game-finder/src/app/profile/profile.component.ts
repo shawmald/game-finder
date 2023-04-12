f6a30bbd12ad1d99cba59364ae2979a3b9ab4ca1
Sydney S <sydney.silverman3@gmail.com>
2023-04-12 12:06:51 -0400

    edit button changes display

--- game-finder/src/app/profile/profile.component.ts
+++ game-finder/src/app/profile/profile.component.ts
@@ -9,9 +9,11 @@ export class ProfileComponent {
   currentUser: string = "test";
   username: string = "test";
 
+  editing: boolean = false;
+
   pfp: string = "";
   displayName: string = "test";
-  contactInformation: string = "test@gmail.com";
+  email: string = "test@gmail.com";
   location: string = "Sarasota, FL";
   status: string = "Looking for Campaign";
   /*tags: ?? = ??;*/
@@ -20,6 +22,33 @@ export class ProfileComponent {
   /*availability: ?? = ??;*/
 
   /*characters: Character[] = [];*/
+
+  /* TODO insert oninit stuff when login & backend is implemented */
+
+  /**
+   * switch to editing mode,
+   * changes editing to true
+   */
+  edit() {
+    this.editing = true;
+  }
+
+  /**
+   * stop editing, values remain unchanged
+   */
+  cancel() {
+    /* TODO reset values if necessary */
+    this.editing = false;
+  }
+
+  /**
+   * stop editing, save new values and update in database
+   */
+  save() {
+    /* TODO save new values, update in database */
+    this.editing = false;
+  }
+
 
   getPFP(): string {
     var dataURI = "";
