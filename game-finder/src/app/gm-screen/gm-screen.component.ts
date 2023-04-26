68c6c2f693bdd488eec78b8205a3bcb03ab7ddc7
Sydney S <sydney.silverman3@gmail.com>
2023-04-26 17:02:03 -0400

    got stats working with an array

--- game-finder/src/app/gm-screen/gm-screen.component.ts
+++ game-finder/src/app/gm-screen/gm-screen.component.ts
@@ -7,7 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./gm-screen.component.css']
 })
 export class GmScreenComponent {
-  npcs: NPC[] = Array<NPC>(12);
+  npcs: NPC[] = Array<NPC>( new NPC(), new NPC(), new NPC(), new NPC() );
+
+  add( name:string='Name', level:number=1, job:string='Class', str:number=10, dex:number=10, con:number=10, int:number=10, wis:number=10, cha:number=10, notes:string='notes' ): void {
+    if( this.npcs.length < 12 ){
+      this.npcs.push( new NPC( name, level, job, str, dex, con, int, wis, cha, notes) );
+    }
+  }
+
+  remove( index:number ): void {
+    if( index < this.npcs.length ){
+      delete this.npcs[index];
+    }
+  }
+}
+
+export class NPC {
+  name: string;
+  level: number;
+  class: string;
+  stats: number[];
+  //stats: Map<string,number>;
+  notes: string;
+
+  /**
+   * Create a new NPC. Default values used if nothing is provided
+   * 
+   * @param name  (string) default = 'Name'
+   * @param level (number) default = 1
+   * @param job   (string) default = 'Class'
+   * @param str   (number) default = 10
+   * @param dex   (number) default = 10
+   * @param con   (number) default = 10
+   * @param int   (number) default = 10
+   * @param wis   (number) default = 10
+   * @param cha   (number) default = 10
+   * @param notes (string) default = 'notes'
+   */
+  constructor( name:string='Name', level:number=1, job:string='Class', str:number=10, dex:number=10, con:number=10, int:number=10, wis:number=10, cha:number=10, notes:string='notes' ){
+    this.name = name;
+    this.level = level;
+    this.class = job;
+    this.stats = [ str, dex, con, int, wis, cha ];
+    /*
+    this.stats = new Map<string,number>();
+    this.stats.set("str", str);
+    this.stats.set("dex", dex);
+    this.stats.set("con", con);
+    this.stats.set("int", int);
+    this.stats.set("wis", wis);
+    this.stats.set("cha", cha);
+    */
+    this.notes = notes;
+  }
 
   /**
    * 
@@ -27,6 +79,7 @@ export class GmScreenComponent {
    * Test calcMod() with inputs 1-30
    * @returns (string)
    */
+  /*
   test_calcMod(): string {
     let output: string = ``;
 
@@ -36,20 +89,6 @@ export class GmScreenComponent {
 
     return output;
   }
-
-}
-
-export class NPC {
-  name: string = '';
-  class: string = '';
-  stats: Map<string,number> = new Map<string,number>([
-    ['Str', 10],
-    ['Dex', 10],
-    ['Con', 10],
-    ['Int', 10],
-    ['Wis', 10],
-    ['Cha', 10]
-  ]);
-  info: string = '';
+  */
 
 }
