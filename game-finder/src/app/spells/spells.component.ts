e89c6ebfb8397bbf6d7b5c68b386752f1c930807
Ariella Russin <ariella@russin.us>
2023-05-15 14:37:40 -0400

    got some stuff going but i have a page not found error now

--- game-finder/src/app/spells/spells.component.ts
+++ game-finder/src/app/spells/spells.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-spells',
   templateUrl: './spells.component.html',
   styleUrls: ['./spells.component.css']
 })
-export class SpellsComponent {
 
+export class SpellsComponent  implements OnInit {
+  spells: any[] = [];
+  selectedClasses: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchSpells();
+  }
+
+  fetchSpells() {
+    // Make an HTTP request to the backend API to retrieve the filtered spells based on selected classes
+    const selectedClassesQuery = this.selectedClasses.join(',');
+    const url = `http://34.30.183.36:80/spells?classes=${selectedClassesQuery}`;
+    this.http.get<any[]>(url)
+      .subscribe(data => {
+        this.spells = data;
+      });
+  }
+
+  onClassSelectionChange() {
+    this.fetchSpells();
+  }
 }
